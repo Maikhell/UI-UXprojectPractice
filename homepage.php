d070b6eafc6b8a57579c729ceac4e8d81b490330
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-03-02 01:39:33 +0800

    Added color scheme/fixing layout

--- homepage.php
+++ homepage.php
@@ -10,32 +10,43 @@
 </head>
 
 <body>
-    <div class="container">
-        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="./images/clay-banks-hwLAI5lRhdM-unsplash.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="./images/jezael-melgoza-NiyRORf8d8I-unsplash.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="./images/jezael-melgoza-layMbSJ3YOE-unsplash.jpg" class="d-block w-100" alt="...">
+    <section class="first-page">
+        <div class="container">
+            <div class="description-title">
+                <h2 class="title">Tokyo</h2>
+                <p>This is the short description for the destination</p>
+            </div>
+            <div class="carousel-container">
+                <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="./images/clay-banks-hwLAI5lRhdM-unsplash.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./images/jezael-melgoza-NiyRORf8d8I-unsplash.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="./images/jezael-melgoza-layMbSJ3YOE-unsplash.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
-    </div>
+    </section>
 
+    <section class="second-half">
+
+    </section>
 </body>
 
-<script src="./bootstrap-5.3.1-dist/js/bootstrap.min.jsS"></script>
+<script src="./bootstrap-5.3.1-dist/js/bootstrap.js"></script>
 
 </html>
